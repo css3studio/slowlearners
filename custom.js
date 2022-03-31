f499eb6163f3d026c89831bf5f097a3cbc67e144
{css3studio} <css3studio@gmail.com>
2022-03-31 23:35:08 +0800

    에티켓-script

--- custom.js
+++ custom.js
@@ -34,62 +34,19 @@ $(document).ready(function() {
 	//Notice 게시판 뷰 구조변경
 	$('.board_view .board_summary .author .date').before($('.board_view header p a'));
 
-
-
-/*
-    //메인 홈 > 지점 소개 및 예약
-	var branches = {
-		"사당" : "#s202102155aa89ede2ed9a",
-		"선릉" : "#s202102174f98591002401",
-		"수원역" : "#s20210218b328c8f043424",
-		"청주상당" : "#s202102184128416a16fd8",
-		"인덕원" : "#s202102182ae4bbdb11a68",
-		"합정" : "#s2021021861818badf2940",
-		"가산디지털단지역" : "#s20210218cb3a3231852aa",
-		"마곡" : "#s202102183367b249b0b9b",
-		"여의도" : "#s2021021885dec74fb36fd"
-	};
-
-	var map_data_area = $('<div class="map-data-area"/>');
-	$('#s20210215159c93c980d75').after(map_data_area);
-	map_data_area.css({
-		position : 'relative'
+	//에티켓 탭메뉴 레이어 전환
+	var sections = ['#s20220331c93b202b05422','#s2022033123d7fd2520021','#s20220331a893966028ed7','#s202203317eac794233917'];
+	$('.tabmenu-etiquette li a').on("click",function(event){
+		$(this).parentsUntil('ul').find('li').removeClass("current");
+		$(this).parent().addClass("current");
+		var idx = $(this).parent();
+		for(var i in sections) {
+			if(idx == i)	$(sections[i]).show();
+			else			$(sections[i]).hide();
+		}
+		event.preventDefault();
 	});
 
-	setTimeout(function(){
-		$.each( branches, function( key, value ) {
-			map_data_area.append($(value));
-			$(value).css({
-				position : 'absolute',
-				top : 0,
-				left : 0,
-				width: '100%'
-			});
-			if(key == "사당")
-		  		$(value).css("z-index", "1");
-		  	else
-		  		$(value).css("z-index", "-1");
-		});
-	},100);
-
-    $('.ng01 > li > a').bind("click",function(event){
-        if($('.ng01').hasClass('openMenu')){
-            $('.ng01').removeClass('openMenu')
-        }else{
-            $('.ng01').addClass('openMenu')
-        }
-        $(".ng01 > li").removeClass("active");
-        $(this).parent().addClass("active");
-        var target = $(this).attr('data-id');
-		$.each( branches, function( key, value ) {
-			if(value == target)
-		  		$(value).css("z-index", "1");
-		  	else
-		  		$(value).css("z-index", "-1");
-		});
-        return false;
-    });
-*/
     var dw = viewport().width;
 	if(dw <= 768){	//모바일
 		$("body").removeClass('pc');
@@ -107,25 +64,11 @@ $(document).ready(function() {
 
 //PC버젼 초기화
 function init_pc(){
-	/*
-	if($('.slider-main .slider .slick-list').length > 0){
-		$('.slider-main .slider').slick('unslick');
-	}*/
+
 }
 
 //모바일 버젼 초기화
 function init_mobile(){
-	/*
-    $('.slider-main .slider').slick({
-	  dots: true,
-	  arrows: false,
-	  infinite: true,
-	  speed: 300,
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  variableWidth: true,
-	  draggable: true
-    */
 
 }
 
