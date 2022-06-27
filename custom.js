3a33de0dcec4e9595772f006935686904de240c3
{css3studio} <css3studio@gmail.com>
2022-06-27 16:21:34 +0800

    first commit

--- custom.js
+++ custom.js
@@ -1,13 +1,10 @@
 /*
-Theme Name: korea women campaign
+Theme Name: slowlearners
 Author: css3studio
 Version:1.0
 */
 
 var device_status = "";
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
 
 $(window).resize(function() {
 	var dw = viewport().width;
@@ -44,40 +41,6 @@ $(window).scroll(function(e){
   */
 $(document).ready(function() {
 
-	//모금액 카운팅
-	var target =  Number($('ul.collection-data li.target span').text());
-	var collection = Number($('ul.collection-data li.collection span').text());
-	var donator = Number($('ul.collection-data li.donator span').text());
-	var finish_day = $('ul.collection-data li.finish span').text();
-	var collection_percent = Math.round(collection/target*100);
-
-	var masTime = new Date(finish_day);
-	var todayTime = new Date();
-	var diff = masTime - todayTime;
-	var diffDay = Math.floor(diff / (1000*60*60*24));
-
-	$('.collection-bulletin dl.collection dd > span em').text(collection_percent);
-	$('.collection-bulletin dl.donator dd > div span').text(numberWithCommas(donator));
-	$('.collection-bulletin dl.period dd > div span').text(diffDay);
-
-	$({ val : 0 }).animate({ val : collection }, {
-		duration: 2000,
-		step: function() {
-			var num = numberWithCommas(Math.floor(this.val));
-			$('.collection-bulletin dl.collection dd > div b').text(num);
-		},
-		complete: function() {
-			var num = numberWithCommas(Math.floor(this.val));
-			$('.collection-bulletin dl.collection dd > div b').text(num);
-			$('.collection-bulletin dl.collection dd > span').show();
-		}
-	});
-	$('.progresive-bar .inner-bar').animate({
-		width: collection_percent + '%'
-	}, 2000, function() {
-		// Animation complete.
-	});
-
     var dw = viewport().width;
 	if(dw <= 768){	//모바일
 		$("body").removeClass('pc');
@@ -91,62 +54,18 @@ $(document).ready(function() {
 		device_status = "pc";
 	}
 
-	$(".campaign-carousel .slider").lightGallery({
-		thumnail: true,
-	});
-
-	$('.campaign-carousel .slider').slick({
-		dots: false,
-		arrows: true,
-		slidesToShow: 4,
-		slidesToScroll: 1,
-		variableWidth: true
-	});
 });
 
 //PC버젼 초기화
 function init_pc(){
-	$('btn-mobile-menu').off();
+
 }
 
 //모바일 버젼 초기화
 function init_mobile(){
-	//헤더 LNB 메뉴(mobile)
-	$(".btn-mobile-menu").on("click",function(event){
-		if($(".campaign-header01").hasClass('open-mobile-menu')){
-			$(".campaign-header01").removeClass('open-mobile-menu');
-		}else{
-			$(".campaign-header01").addClass('open-mobile-menu');
-		}
-		event.preventDefault();
-	});
-	setInterval(function() {
-		if (didScroll) {
-			hasScrolled();
-			didScroll = false;
-		}
-	}, 250);
 
 }
 
-function hasScrolled() {
-    var st = $(this).scrollTop();
-
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-
-
-    if (st > lastScrollTop){
-        // Scroll Down
-        $('.campaign-header01 .btn-donate').removeClass('up').addClass('down');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('.campaign-header01 .btn-donate').removeClass('down').addClass('up');
-        }
-    }
-    lastScrollTop = st;
-}
 
 function numberWithCommas(x) {
 	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
